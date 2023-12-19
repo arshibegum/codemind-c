55d859dad4ccd9674aab95f01184ed8c8515eb5d
arshibegum <147418400+arshibegum@users.noreply.github.com>
2023-12-19 10:09:59 +0530

    #include<stdio.h> int main() {     int N,i;     int arr[N];     scanf("%d",&N);     for(i=0;i<=N;i++)     scanf("%d ",&arr[i]);     int sum=0;     for(i=0;i<=N;i++)     {         if(arr[i]%2==1)         sum=sum+arr[i];     }     printf("%d",sum); }

---
+++ Odd_Sum.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int N,i;
+    int arr[N];
+    scanf("%d",&N);
+    for(i=0;i<=N;i++)
+    scanf("%d ",&arr[i]);
+    int sum=0;
+    for(i=0;i<=N;i++)
+    {
+        if(arr[i]%2==1)
+        sum=sum+arr[i];
+    }
+    printf("%d",sum);
+}  
